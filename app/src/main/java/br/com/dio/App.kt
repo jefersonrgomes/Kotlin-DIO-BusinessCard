36ab8282fea4f10ecc2cbcc37f02ecc8b7a5da62
jefersonrgomes <jeferson.rgomes@hotmail.com>
2021-07-15 03:52:58 -0300

    Criação de diretórios MVVM, testes ADV ok, implementação do viewBinding

---
+++ app/src/main/java/br/com/dio/App.kt
@@ -0,0 +1,8 @@
+package br.com.dio
+
+import android.app.Application
+
+class App : Application()
+{
+
+}
--- app/src/main/java/br/com/dio/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/ui/MainActivity.kt
@@ -1,11 +1,15 @@
-package br.com.dio
+package br.com.dio.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
